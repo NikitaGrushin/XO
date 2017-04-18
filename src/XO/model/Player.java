52473975cc379308b98473c4addba851a6510924
Nikita Grushin <Die_for_metal@mail.ru>
2017-04-18 15:22:31 +0300

    Create Figure

--- src/XO/model/Player.java
+++ src/XO/model/Player.java
@@ -2,4 +2,24 @@ package XO.model;
 
 
 public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player (final String name,
+                   final Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Figure getFigure(){
+        return this.figure;
+    }
+
+
 }
