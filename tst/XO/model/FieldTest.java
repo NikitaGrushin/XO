ef6a941c31c02215cf7841cd88969f302c7e626d
Nikita Grushin <Die_for_metal@mail.ru>
2017-04-19 14:28:00 +0300

    FieldTests checks for exceptions have been added

--- tst/XO/model/FieldTest.java
+++ tst/XO/model/FieldTest.java
@@ -1,5 +1,6 @@
 package XO.model;
 
+import XO.model.exceptions.AlreadyOccupiedException;
 import XO.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -106,5 +107,19 @@ public class FieldTest {
             field.getFigure(inputPoint);
             fail();
         } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void setFigureWhenPointIsAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+        fail();
+        } catch (final AlreadyOccupiedException e){}
     }
 }
